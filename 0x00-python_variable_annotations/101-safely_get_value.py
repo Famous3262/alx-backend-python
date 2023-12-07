5ecaeaf2087615a66cf5cd8fede81857b5e78885
Famouus Bernard <famousbernard@gmail.com>
2023-12-07 13:04:29 +0000

    variable annotation

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""adding type annotations to the function"""
+
+T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
+
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    """annotations of the function"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" A type-annotated function that takes a float as argument
+and returns a function that multiplies a float by multiplier.
+"""
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+   """Returns a function that multiplies a float by multiplier"""
+    def multiply(n: float) -> float:
+        return n * multiplier
+
+    return multiply
