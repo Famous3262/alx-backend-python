8774063d04a4f0ac6acaf6bcb6cd7a84c7f59cdc
Famouus Bernard <famousbernard@gmail.com>
2023-12-11 12:30:28 +0000

    python async function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" An asynchronous coroutine
+that takes in an integer argument
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Waits for some seconds """
+    delay = random.uniform() * max_delay
+    await asyncio.sleep(delay)
+    return delay
