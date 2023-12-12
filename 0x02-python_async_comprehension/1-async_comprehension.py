6a22eb80364b92df7ca4b33ae7e526075761dcbf
Famouus Bernard <famousbernard@gmail.com>
2023-12-12 13:10:30 +0000

    successful

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,11 +4,13 @@ Return the 10 random numbers collected using
 an async comprehensing over async_generator
 """
 
+
 from typing import List
+from importlib import import_module as using
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Return the 10 random numbers"""
     rand_numbers = [num async for num in async_generator()]
     return rand_numbers
